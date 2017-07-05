0389c5c7cc34d3439559922ae538ea5fe5c26688
Patrick <patfedigan@gmail.com>
2017-07-05 14:18:55 -0700

    Fixing and cleaning

--- docs.js
+++ docs.js
@@ -15,9 +15,10 @@ angular.module('timer-demo',['timer']).controller('TimerDemoController',['$scope
     };
 
     $scope.currentYear = (new Date).getFullYear();
-    $scope.startTime = (new Date($scope.currentYear, 0, 1)).getTime();
+    $scope.startTime = sinceAnn;
     $scope.endYear = $scope.currentYear+1;
     $scope.endTime = (new Date($scope.endYear, 0, 1)).getTime();
+
 
     $scope.callbackTimer={};
     $scope.callbackTimer.status='Running';
@@ -27,4 +28,12 @@ angular.module('timer-demo',['timer']).controller('TimerDemoController',['$scope
         $scope.callbackTimer.callbackCount++;
         $scope.$apply();
     };
+
+    function monthDiff(d1, d2) {
+    var months;
+    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months -= d1.getMonth() + 1;
+    months += d2.getMonth();
+    return months <= 0 ? 0 : months;
+}
 }]);
